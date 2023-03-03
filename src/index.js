5c40fed6cc57d4f433673112aad260b191eafe67
traviscult <60018617+traviscult@users.noreply.github.com>
2023-03-03 15:02:54 -0600

    fixed typo in index js

--- src/index.js
+++ src/index.js
@@ -3,13 +3,13 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import { Auth0Provider } from "@auth0/auth0-react";
 
-const domain = process.env.React_APP_AUTH0_DOMAIN;
+const domain = process.env.REACT_APP_AUTH0_DOMAIN;
 const clientID = process.env.REACT_APP_AUTH0_CLIENT_ID;
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <Auth0Provider
-    doamin={domain}
+    domain={domain}
     clientId={clientID}
     redirectURI={window.location.origin}
   >
