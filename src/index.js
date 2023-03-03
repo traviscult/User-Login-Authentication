faa33ae93d7d3bc22f38c1f2664d8a780d4553ad
traviscult <60018617+traviscult@users.noreply.github.com>
2023-03-03 14:54:42 -0600

    imported auth0provider to index js

--- src/index.js
+++ src/index.js
@@ -1,10 +1,18 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
+import { Auth0Provider } from "@auth0/auth0-react";
+
+const domain = process.env.React_APP_AUTH0_DOMAIN;
+const clientID = process.env.REACT_APP_AUTH0_CLIENT_ID;
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
+  <Auth0Provider
+    doamin={domain}
+    clientId={clientID}
+    redirectURI={window.location.origin}
+  >
     <App />
-  </React.StrictMode>
+  </Auth0Provider>
 );
