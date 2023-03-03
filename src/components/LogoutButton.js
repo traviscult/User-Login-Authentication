7b7d96990c51b0f41d63b139ceab6fab360e1ffd
traviscult <60018617+traviscult@users.noreply.github.com>
2023-03-03 17:11:06 -0600

    used isAuthenticated to display only login or logout

--- src/components/LogoutButton.js
+++ src/components/LogoutButton.js
@@ -2,8 +2,8 @@ import React from "react";
 import { useAuth0 } from "@auth0/auth0-react";
 
 const LogoutButton = () => {
-  const { logout } = useAuth0();
-  return <button onClick={() => logout()}>Log Out</button>;
+  const { logout, isAuthenticated } = useAuth0();
+  return isAuthenticated && <button onClick={() => logout()}>Log Out</button>;
 };
 
 export default LogoutButton;
